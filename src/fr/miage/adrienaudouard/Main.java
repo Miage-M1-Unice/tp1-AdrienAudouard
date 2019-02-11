8b436ed57da69b7cb4a9f4b48ed2b3f28a32c59d
Adrien Audouard <adrien.audouard@gmail.com>
2019-02-11 15:12:20 +0100

    init projet

---
+++ src/fr/miage/adrienaudouard/Main.java
@@ -0,0 +1,8 @@
+package fr.miage.adrienaudouard;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/fr/miage/adrienaudouard/exercice1/QuestionA.java
@@ -0,0 +1,5 @@
+package fr.miage.adrienaudouard.exercice1;
+
+public class QuestionA {
+    
+}
