63fff7719b99a3617f91f932b99bcdb16b5d289c
Adrien Audouard <adrien.audouard@gmail.com>
2019-02-11 15:19:40 +0100

    Question 1.a

--- src/fr/miage/adrienaudouard/exercice1/QuestionA.java
+++ src/fr/miage/adrienaudouard/exercice1/QuestionA.java
@@ -1,5 +1,33 @@
 package fr.miage.adrienaudouard.exercice1;
 
+import java.io.File;
+import java.util.Arrays;
+
 public class QuestionA {
-    
+    public static void main(String[] args) {
+
+        System.out.println("Question 1.a:");
+        listerRepertoireCourant();
+
+        System.out.println("Question 1.b:");
+        parcoursEnProfondeur();
+    }
+
+    private static void listerRepertoireCourant() {
+        File f = new File(".");
+
+        String[] contenu = f.list();
+
+        for (String s : contenu) {
+            System.out.println(s);
+        }
+    }
+
+    private static void parcoursEnProfondeur() {
+        File f = new File("");
+
+        f.listFiles();
+
+        System.out.println();
+    }
 }
